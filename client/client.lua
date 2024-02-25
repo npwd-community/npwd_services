892be9aea27e67760a31dbfc35a2cd4fbfbf191e
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2024-02-25 14:17:00 +0100

    feat: ND framework support
    kinda did a little rewrite too so it wouldn't turn into a mess

--- client/client.lua
+++ client/client.lua
@@ -1,42 +1,55 @@
-local isESX = GetResourceState("es_extended") ~= "missing"
-local isQB = GetResourceState("qb-core") ~= "missing"
-local FrameworkObj = {}
-if isESX and isQB then
-	print("[ERROR] You are using both ESX and QB-Core, please remove one of them.")
-elseif isESX then
-	FrameworkObj = exports["es_extended"]:getSharedObject()
-elseif isQB then
-	FrameworkObj = exports["qb-core"]:GetCoreObject()
-end
+local supportedFrameworks = {"es_extended", "qb-core", "ND_Core"}
 
-if isESX then
-	RegisterNUICallback("npwd:services:getPlayers", function(_, cb)
-		FrameworkObj.TriggerServerCallback("npwd:services:getPlayers", function(PlayerList)
-			cb({ status = "ok", data = PlayerList })
-		end)
-	end)
-	
-	
-	RegisterNUICallback("npwd:services:callPlayer", function(data, cb)
-		-- print(data.job) job of player being called
-		exports.npwd:startPhoneCall(tostring(data.number))
-		cb({})
-	end)
-end
+local frameworks = {
+    ["es_extended"] = function()
+        local ESX = exports["es_extended"]:getSharedObject()
 
-if isQB then 
-	RegisterNUICallback("npwd:services:getPlayers", function(_, cb)
-		TriggerServerEvent("npwd:services:getPlayers")
-		RegisterNetEvent("npwd:services:sendPlayers", function(players)
-			cb({ status = "ok", data = players })
-		end)
-	end)
-	
-	
-	RegisterNUICallback("npwd:services:callPlayer", function(data, cb)
-		-- print(data.job) job of player being called
-		exports.npwd:startPhoneCall(tostring(data.number))
-		cb({})
-	end)
-end
+        RegisterNUICallback("npwd:services:getPlayers", function(_, cb)
+            ESX.TriggerServerCallback("npwd:services:getPlayers", function(PlayerList)
+                cb({ status = "ok", data = PlayerList })
+            end)
+        end)
+        
+        RegisterNUICallback("npwd:services:callPlayer", function(data, cb)
+            -- print(data.job) job of player being called
+            exports.npwd:startPhoneCall(tostring(data.number))
+            cb({})
+        end)
+    end,
+    ["qb-core"] = function()
+        RegisterNUICallback("npwd:services:getPlayers", function(_, cb)
+            TriggerServerEvent("npwd:services:getPlayers")
+            RegisterNetEvent("npwd:services:sendPlayers", function(players)
+                cb({ status = "ok", data = players })
+            end)
+        end)
+        
+        RegisterNUICallback("npwd:services:callPlayer", function(data, cb)
+            -- print(data.job) job of player being called
+            exports.npwd:startPhoneCall(tostring(data.number))
+            cb({})
+        end)
+    end,
+    ["ND_Core"] = function()
+        RegisterNUICallback("npwd:services:getPlayers", function(_, cb)
+            TriggerServerEvent("npwd:services:getPlayers")
+            RegisterNetEvent("npwd:services:sendPlayers", function(players)
+                cb({ status = "ok", data = players })
+            end)
+        end)
+        
+        RegisterNUICallback("npwd:services:callPlayer", function(data, cb)
+            -- print(data.job) job of player being called
+            exports.npwd:startPhoneCall(tostring(data.number))
+            cb({})
+        end)
+    end
+}
 
+for i=1, #supportedFrameworks do
+    local fw = supportedFrameworks[i]
+    if GetResourceState(fw) ~= "missing" then
+        frameworks[fw]()
+        break
+    end
+end
