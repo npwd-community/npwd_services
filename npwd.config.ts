7d2f2caad942c8083c4ad513d6a39a1a0769fc96
itschip <chip@projecterror.dev>
2024-03-02 17:28:47 +0100

    fix: update deps and config

--- npwd.config.ts
+++ npwd.config.ts
@@ -1,16 +1,14 @@
-import App from './src/App';
-import { ServicesIcon } from './icon';
-import { theme } from './src/app.theme';
+import App from "./src/App";
+import { ServicesIcon } from "./icon";
+import { theme } from "./src/app.theme";
 
-export const externalAppConfig = () => ({
-  id: 'services',
-  nameLocale: 'Services',
-  color: '#fff',
-  backgroundColor: '#333',
-  path: '/services',
+export default (settings: any) => ({
+  id: "services",
+  nameLocale: "Services",
+  color: "#fff",
+  backgroundColor: "#333",
+  path: "/services",
   icon: ServicesIcon,
   app: App,
   theme: theme,
 });
-
-export default externalAppConfig;
